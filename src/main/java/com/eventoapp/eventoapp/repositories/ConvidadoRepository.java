e21ce2096c68d25da0b7d77a4e88a44d90f2a484
Felipe Mendes <felipemendes1994@gmail.com>
2018-05-14 14:54:38 -0300

    Primeiro Commit

---
+++ src/main/java/com/eventoapp/eventoapp/repositories/ConvidadoRepository.java
@@ -0,0 +1,17 @@
+package com.eventoapp.eventoapp.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eventoapp.eventoapp.models.Convidado;
+import com.eventoapp.eventoapp.models.Evento;
+
+@Repository
+public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
+	
+	List<Convidado> findByEvento(Evento evento);
+	
+	Convidado findByRg(String rg);
+}
---
+++ src/main/java/com/eventoapp/eventoapp/repositories/EventoRepository.java
@@ -0,0 +1,12 @@
+package com.eventoapp.eventoapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eventoapp.eventoapp.models.Evento;
+
+@Repository
+public interface EventoRepository extends CrudRepository<Evento, String>{
+
+	Evento findByCodigo(long codigo);
+}
