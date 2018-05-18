b8e1dc0e7f1f22a86d250bd403a5f2aa7f850b6e
Felipe Mendes <felipemendes1994@gmail.com>
2018-05-18 00:07:30 -0300

    Ajustando configuracao de dialete MySQL

--- src/main/java/com/eventoapp/eventoapp/configuration/ProdDataConfiguration.java
+++ src/main/java/com/eventoapp/eventoapp/configuration/ProdDataConfiguration.java
@@ -7,6 +7,9 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 @Profile("prod")
@@ -27,4 +30,15 @@ public class ProdDataConfiguration {
 
         return basicDataSource;
     }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 }
